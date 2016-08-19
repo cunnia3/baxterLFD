95dc14a837a5806b9d7c1ff741d2c8be94db83bc
cunnia3 <andrew64ce@gmail.com>
2016-08-19 13:23:58 -0400

    First commit

---
+++ recorder.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from geometry_msgs.msg import Pose
+import rospy
+from tf import TransformListener
+import time
+import csv
+import thread
+
+def stopCheckThread(list):
+    """Thread used to check to see if the user has pressed enter"""
+    raw_input("Recording, press enter to stop recording")
+    list.append(None)
+
+class RecorderNode:
+    """This node is responsible for recording the pose of the end effector
+    over the course of a human demonstration, it will log the poses over time
+    as a csv with each line containing a pose. I.E. a line will look like:
+    X,Y,Z,quat1,quat2,quat3,quat4"""
+    
+    def __init__(self, csvName = "recording1.csv"):
+        rospy.init_node('my_node_name')
+        self.tf = TransformListener()
+        self.poseList = [] #  X,Y,Z,quat1,quat2,quat3,quat4
+        self.csvName = csvName
+
+    def logPose(self):
+        """get pose from TF tree"""
+        if self.tf.frameExists("/base") and self.tf.frameExists("/right_wrist"):
+            t = self.tf.getLatestCommonTime("/base", "/right_wrist")
+            position, quaternion = self.tf.lookupTransform("/base", "/right_wrist", t)
+          
+            poseInstance = [position[0], position[1], position[2],quaternion[0],quaternion[1],quaternion[2],quaternion[3]]
+            self.poseList.append(poseInstance)
+            return
+    
+    def writeCSV(self):
+        myfile = open(self.csvName, 'wb')
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(self.poseList)
+            
+    def runNode(self):
+        list = []
+        thread.start_new_thread(stopCheckThread, (list,))
+        while not list:
+            self.logPose()
+            time.sleep(.1)
+        self.writeCSV()
+
+myNode = RecorderNode()
+myNode.runNode()
