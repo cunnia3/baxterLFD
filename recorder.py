08c4f4bdab64504870ad57b06665cd63adefd69a
cunnia3 <andrew64ce@gmail.com>
2016-08-19 14:06:30 -0400

    Added time recording, removed quotations in csv

--- recorder.py
+++ recorder.py
@@ -15,28 +15,30 @@ class RecorderNode:
     """This node is responsible for recording the pose of the end effector
     over the course of a human demonstration, it will log the poses over time
     as a csv with each line containing a pose. I.E. a line will look like:
-    X,Y,Z,quat1,quat2,quat3,quat4"""
+    time,X,Y,Z,quat1,quat2,quat3,quat4"""
     
     def __init__(self, csvName = "recording1.csv"):
         rospy.init_node('my_node_name')
         self.tf = TransformListener()
         self.poseList = [] #  X,Y,Z,quat1,quat2,quat3,quat4
         self.csvName = csvName
+        self.startTime = time.time()
 
     def logPose(self):
         """get pose from TF tree"""
         if self.tf.frameExists("/base") and self.tf.frameExists("/right_wrist"):
             t = self.tf.getLatestCommonTime("/base", "/right_wrist")
             position, quaternion = self.tf.lookupTransform("/base", "/right_wrist", t)
-          
-            poseInstance = [position[0], position[1], position[2],quaternion[0],quaternion[1],quaternion[2],quaternion[3]]
+            currentTime = time.time() - self.startTime
+            poseInstance = [currentTime, position[0], position[1], position[2],quaternion[0],quaternion[1],quaternion[2],quaternion[3]]
             self.poseList.append(poseInstance)
             return
     
     def writeCSV(self):
         myfile = open(self.csvName, 'wb')
-        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(self.poseList)
+        wr = csv.writer(myfile)
+        for poseInstance in self.poseList:
+            wr.writerow(poseInstance)
             
     def runNode(self):
         list = []
