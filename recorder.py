4f6f3116ef060540d06804da2c143960a9f1747e
cunnia3 <andrew64ce@gmail.com>
2016-08-20 15:02:06 -0400

    added time and euler angle recording

--- recorder.py
+++ recorder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
+import tf
 from tf import TransformListener
 import time
 import csv
@@ -15,7 +16,7 @@ class RecorderNode:
     """This node is responsible for recording the pose of the end effector
     over the course of a human demonstration, it will log the poses over time
     as a csv with each line containing a pose. I.E. a line will look like:
-    time,X,Y,Z,quat1,quat2,quat3,quat4"""
+    time,X,Y,Z,roll,pitch,yaw"""
     
     def __init__(self, csvName = "recording1.csv"):
         rospy.init_node('my_node_name')
@@ -29,8 +30,12 @@ class RecorderNode:
         if self.tf.frameExists("/base") and self.tf.frameExists("/right_wrist"):
             t = self.tf.getLatestCommonTime("/base", "/right_wrist")
             position, quaternion = self.tf.lookupTransform("/base", "/right_wrist", t)
+            euler = tf.transformations.euler_from_quaternion(quaternion)
+            roll = euler[0]
+            pitch = euler[1]
+            yaw = euler[2]
             currentTime = time.time() - self.startTime
-            poseInstance = [currentTime, position[0], position[1], position[2],quaternion[0],quaternion[1],quaternion[2],quaternion[3]]
+            poseInstance = [currentTime, position[0], position[1], position[2], roll, pitch, yaw]
             self.poseList.append(poseInstance)
             return
     
